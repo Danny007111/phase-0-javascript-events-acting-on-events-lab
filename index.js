064290cc897e3173e8e42a75ddd4cd722440b065
Danny Rodriguez <danny007111@hotmail.com>
2022-04-26 12:08:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // Your code here
+const dodger = document.getElementById("dodger");
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-      const leftNumbers = dodger.style.left.replace("px", "");
-      const left = parseInt(leftNumbers, 10);
-  
-      dodger.style.left = `${left - 1}px`;
-    } 
-    
-  });
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if(e.which === 39){
+    moveDodgerRight();
+  } else if (e.which === 37) {
+    moveDodgerLeft();
+}});
